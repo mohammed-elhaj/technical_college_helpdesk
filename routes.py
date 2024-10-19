8dc22191aea52a093717fb7cc4411994c2f2b671
mohammed-elhaj <78878412+mohammed-elhaj@users.noreply.github.com>
2024-10-19 20:54:10 +0200

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,3 +1,7 @@
+# File: routes.py
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 import os
 import plotly.express as px
